eddaba1b09f9b09a788f278b8a4c9eafe805d142
Minh <nguyengiangminh99@gmail.com>
2024-05-07 20:47:37 +0700

    create project

---
+++ src/test/java/fit/se2/springboot/SpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package fit.se2.springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
