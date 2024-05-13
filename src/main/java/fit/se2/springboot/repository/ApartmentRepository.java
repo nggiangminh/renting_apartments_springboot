15b04577c594dfd0d4cd86ee33dc89dde55db32b
Minh <nguyengiangminh99@gmail.com>
2024-05-13 20:56:24 +0700

    delete all

--- src/main/java/fit/se2/springboot/repository/ApartmentRepository.java
+++
@@ -1,7 +0,0 @@
-package fit.se2.springboot.repository;
-
-import fit.se2.springboot.model.Apartment;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ApartmentRepository extends JpaRepository<Apartment, Long> {
-}
