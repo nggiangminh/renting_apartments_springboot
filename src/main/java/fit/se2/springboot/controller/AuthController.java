1f5259c4e5fcb30c3a9e885209b10b0937e07ee5
nhdang16 <nhda6k53@gmail.com>
2024-05-14 09:21:53 +0700

    dang nhap

---
+++ src/main/java/fit/se2/springboot/controller/AuthController.java
@@ -0,0 +1,42 @@
+package fit.se2.springboot.controller;
+
+
+import fit.se2.springboot.model.User;
+import fit.se2.springboot.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.List;
+
+@Controller
+public class AuthController {
+
+    @Autowired
+    private UserRepository userRepo;
+
+    @GetMapping("")
+    public String viewHomePage() {
+        return "Home";
+    }
+
+    @GetMapping("/signup")
+    public String showRegistrationForm(Model model) {
+        model.addAttribute("user", new User());
+        return "signup";
+    }
+
+    @PostMapping("/process_signup")
+    public String processSignup(User user) {
+        BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
+        String encodedPassword = passwordEncoder.encode(user.getPassword());
+        user.setPassword(encodedPassword);
+
+        userRepo.save(user);
+
+        return "signup_success";
+    }
+}
--- src/main/java/fit/se2/springboot/model/User.java
+++ src/main/java/fit/se2/springboot/model/User.java
@@ -1,33 +1,41 @@
 package fit.se2.springboot.model;
 
 import jakarta.persistence.*;
+
 import java.util.HashSet;
 import java.util.Set;
 
 @Entity
+@Table(name = "user")
 public class User {
 
+    // Relationship from the first class
+    @OneToMany(mappedBy = "owner")
+    private final Set<Apartment> apartments = new HashSet<>();
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-
-    @Column(nullable = false, unique = true)
-    private String username;
-
     @Column(nullable = false)
     private String password;
-
-    @Column(nullable = false)
-    private String fullName;
-
-    @Column(nullable = false, unique = true)
+    @Column(nullable = false, unique = true, length = 45)
     private String email;
-
+    @Column(nullable = false, length = 20)
+    private String firstName; // First name field from the second class
+    @Column(nullable = false, length = 20)
+    private String lastName;  // Last name field from the second class
+    @Column(nullable = false)
+    private String fullName = firstName + lastName;  // Full name field from the first class
     private String phoneNumber;
 
-    // Add relationship mappings
-    @OneToMany(mappedBy = "owner")
-    private final Set<Apartment> apartments = new HashSet<>();
+    // Getters and setters
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
 
     public String getPassword() {
         return password;
@@ -37,20 +45,36 @@ public class User {
         this.password = password;
     }
 
-    public String getFullName() {
-        return fullName;
-    }
-
-    public void setFullName(String fullName) {
-        this.fullName = fullName;
-    }
-
     public String getEmail() {
         return email;
     }
 
     public void setEmail(String email) {
         this.email = email;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
     }
 
     public String getPhoneNumber() {
@@ -63,13 +87,5 @@ public class User {
 
     public Set<Apartment> getApartments() {
         return apartments;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public String getUsername() {
-        return username;
     }
 }
